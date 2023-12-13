25dbf0a496b4eac23d196171d5dfd21ab4df37f9
John Tyree <152901179+johnLTyree@users.noreply.github.com>
2023-12-13 14:03:08 -0500

    Update Dictionaries.py

--- Dictionaries.py
+++ Dictionaries.py
@@ -1,48 +1,71 @@
+# Dictionary of emails
 emails = {
-            'Anne Stahl' : 'astahl@gmail.com', 
-            'Peter Small': 'peters@yandex.com', 
-            'Mark Steel': 'Mark@steel.com'
-    
-        }
+    'Anne Stahl': 'astahl@gmail.com',
+    'Peter Small': 'peters@yandex.com',
+    'Mark Steel': 'Mark@steel.com'
+}
+
+# Access and print the email for 'Mark Steel'
 print(emails['Mark Steel'])
 
+# Dictionary of Spanish animal names
 spanish_animals = {
-                   'dog': 'el perro', 
-                   'cat': 'el gato', 
-                   'horse': 'el caballo', 
-                   'bird': 'el pajaro'
-                   }
+    'dog': 'el perro',
+    'cat': 'el gato',
+    'horse': 'el caballo',
+    'bird': 'el pajaro'
+}
+
+# Access and print the Spanish name for 'bird'
 print(spanish_animals['bird'])
 
-
+# Empty dictionary for grades
 grades = {}
 
+# Add grades for students
 grades['John'] = 'A-'
 grades['Anne'] = 'B'
+
+# Print the current grades
 print(grades)
 
+# Update Anne's grade
 grades['Anne'] = 'A'
+
+# Print the updated grades
 print(grades)
 
+# Update John's grade using the update method
 grades.update({'John': 'A'})
+
+# Print the updated grades
 print(grades)
 
-len(grades)
+# Get the number of items in the grades dictionary
+print(len(grades))
 
+# Check if 'John' is in the grades dictionary and print his grade
 if 'John' in grades:
     print('John got:', grades['John'])
-    
+
+# Delete John's grade
 del grades['John']
+
+# Print the grades after deleting John's entry
 print(grades)
 
+# Iterate through keys in the grades dictionary and print them
 for el in grades:
     print(el)
-    
-for el in grades.keys(): 
+
+# Alternatively, iterate through keys using the keys() method
+for el in grades.keys():
     print(el)
-    
+
+# Iterate through values in the grades dictionary and print them
 for el in grades.values():
     print(el)
-    
+
+# Iterate through both keys and values and print them
 for person, grade in grades.items():
     print(person, 'got', grade)
